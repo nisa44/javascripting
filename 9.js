0faccb9e958cdb29d0d5fb2d64f05f87156ddf0c
Nikita Maltsev <nisa4414@gmail.com>
2023-05-22 18:45:56 -0700

    initial commit

---
+++ 9.js
@@ -0,0 +1,9 @@
+let fruit = 'orange'
+const more = "The fruit name has more than five characters."
+const less = "The fruit name has five characters or less."
+
+if (fruit.length > 5) {
+  console.log(more)
+} else {
+  console.log(less)
+}
